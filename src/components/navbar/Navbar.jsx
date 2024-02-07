4a9e3d8210b7ba0011dd450534f2be6ed8a8a083
single-byte <joharkhan1999@gmail.com>
2024-02-07 12:43:23 +0500

    Refactor Navbar component to include logo and links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,12 @@
+import Links from "./links/Links";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
 }
